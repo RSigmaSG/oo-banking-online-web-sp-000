fd08182f2be78d8ba9545dbb1bb61431f09ecb98
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-24 05:06:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
   def execute_transaction
     
     if(@status == "pending")
-      if((@sender.balance > @amount) && (!@sender.valid?))
+      if((@sender.balance > @amount) && (@sender.valid?))
         
         @receiver.deposit(@amount)
         @sender.deposit(0-@amount)
