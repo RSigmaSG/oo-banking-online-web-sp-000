73c85598a96b05e4c72afdb46be7d5a4d421691c
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-24 04:46:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,14 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :reciever, :status, :transfer_amount
+  
+  def initialize(sender, reciever, amount)
+  
+    @sender = sender
+    @reciever = reciever
+    @transfer_amount = amount
+    @status = pending
+  
+  end
+  
 end
