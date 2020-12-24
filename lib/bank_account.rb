9db27c3c47e4b0ea1ab50d948bba1a1577d7cc7f
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-24 04:44:04 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -27,7 +27,13 @@ class BankAccount
   
   def valid?
     
-    (status == "open" && balance > 0) ? true : false
+    (@status == "open" && @balance > 0) ? true : false
+    
+  end
+  
+  def close_account
+    
+    @status = "closed"
     
   end
 
