24228ea7fe7f188d878bfd59524cc41bd47ede6f
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-24 05:01:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,6 +23,7 @@ class Transfer
       if(@sender.balance < @amount)
         
         puts "Transaction rejected. Please check your account balance."
+        @status = "rejected"
         return "Transaction rejected. Please check your account balance."
         
       else
